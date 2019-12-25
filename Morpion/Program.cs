5de12bc32a87b78a76e96ae15b003c9a731cae88
Simon Scatton <simon.scatton@epita.fr>
2019-12-25 22:20:37 +0100

    Initial commit

---
+++ Morpion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Morpion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}    
